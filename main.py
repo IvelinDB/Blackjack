d055727349b8b9ad8db46a5456dfdd3077d9b325
Ivelin <ivanov.ivelin.2006@gmail.com>
2025-10-24 21:56:26 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,59 @@
+import random
+
+
+def random_cards():
+    """Returns a random card from the deck"""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+
+def score_calculation(cards):
+    """Takes a list of the cards and returns the score calculated"""
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+
+    return sum(cards)
+
+
+def compare_scores(u_score, c_score):
+    """Compare the users score against the computers score"""
+    if u_score == c_score:
+        return "Draw :("
+    elif c_score == 0:
+        return "Lose! Computer has Blackjack :("
+    elif u_score == 0:
+        return "You win with Blackjack 😎"
+    elif u_score > 21:
+        return "You busted. You lose :("
+    elif c_score > 21:
+        return "Computer busted. You won!!"
+    elif u_score > c_score:
+        return "You won!"
+    else:
+        return "You lose :("
+    
+def play_games():
+    user_cards = []
+    computer_cards = []
+    computer_score = -1
+    user_score = -1
+    is_games_over = False
+    
+    for _ in range(2):
+        user_cards.append(random_cards())
+        computer_cards.append(random_cards())
+
+    
+    while not is_games_over:
+        user_score = score_calculation(user_cards)
+        computer_cards = score_calculation(computer_cards)
+        print(f"Your cards: {user_cards}, current score: {user_score}")
+        print(f"Computers first card: {computer_cards[0]}")
+
+        if user_score == 0 or compare_scores == 0 or user_score > 21:
+            is_games_over = True
